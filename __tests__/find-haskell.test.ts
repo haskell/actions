dfe236d1332395ed37bded79a5a3ec72270da16c
Jared Weakly <jweakly@galois.com>
2020-04-30 11:36:10 -0700

    Make updating tests on version bumps unnecessary

--- __tests__/find-haskell.test.ts
+++ __tests__/find-haskell.test.ts
@@ -3,6 +3,11 @@ import {getInput} from '@actions/core';
 import * as supported_versions from '../src/versions.json';
 
 const def = getDefaults();
+const latestVersions = {
+  ghc: supported_versions.ghc[0],
+  cabal: supported_versions.cabal[0],
+  stack: supported_versions.stack[0]
+};
 
 const mkName = (s: string): string =>
   `INPUT_${s.replace(/ /g, '_').toUpperCase()}`;
@@ -25,8 +30,7 @@ describe('actions/setup-haskell', () => {
   afterEach(() => (process.env = OLD_ENV));
 
   it('Parses action.yml to get correct default versions', () => {
-    const defs = {ghc: '8.10.1', cabal: '3.2.0.0', stack: '2.1.3'};
-    forAll(t => expect(def[t].version).toBe(defs[t]));
+    forAll(t => expect(def[t].version).toBe(latestVersions[t]));
   });
 
   it('Supported versions are parsed from JSON correctly', () =>
@@ -56,14 +60,13 @@ describe('actions/setup-haskell', () => {
   });
 
   it('"latest" Versions resolve correctly', () => {
-    const v = {ghc: '8.6.5', cabal: '2.4.1.0', stack: '2.1.3'};
     setupEnv({
-      'stack-version': '2.1',
-      'ghc-version': '8.6',
-      'cabal-version': '2.4'
+      'stack-version': 'latest',
+      'ghc-version': 'latest',
+      'cabal-version': 'latest'
     });
     const options = getOpts(def);
-    forAll(t => expect(options[t].resolved).toBe(v[t]));
+    forAll(t => expect(options[t].resolved).toBe(latestVersions[t]));
   });
 
   it('Enabling stack does not disable GHC or Cabal', () => {
