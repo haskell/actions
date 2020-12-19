f7df097f97c3c23ac2952b2681fd5063c36446dc
Jared Weakly <jweakly@galois.com>
2020-12-19 11:41:30 -0800

    Fix husky/lintstaged (#10)
    * fix husky and lint-staged

--- setup/lib/installer.d.ts
+++ setup/lib/installer.d.ts
@@ -1,6 +1,2 @@
-import type {OS, Tool} from './opts';
-export declare function installTool(
-  tool: Tool,
-  version: string,
-  os: OS
-): Promise<void>;
+import type { OS, Tool } from './opts';
+export declare function installTool(tool: Tool, version: string, os: OS): Promise<void>;
