c6a17e283afcafac2b2e6454b778ce18668739b6
Jared Weakly <jweakly@galois.com>
2021-02-08 15:52:03 -0800

    Fallback to --pre with chocolatey

--- setup/src/installer.ts
+++ setup/src/installer.ts
@@ -187,7 +187,7 @@ async function choco(tool: Tool, version: string): Promise<void> {
   core.info(`Attempting to install ${tool} ${version} using chocolatey`);
   // Choco tries to invoke `add-path` command on earlier versions of ghc, which has been deprecated and fails the step, so disable command execution during this.
   console.log('::stop-commands::SetupHaskellStopCommands');
-  await exec('powershell', [
+  const args = [
     'choco',
     'install',
     tool,
@@ -196,7 +196,9 @@ async function choco(tool: Tool, version: string): Promise<void> {
     '-m',
     '--no-progress',
     '-r'
-  ]);
+  ];
+  if ((await exec('powershell', args)) !== 0)
+    await exec('powershell', [...args, '--pre']);
   console.log('::SetupHaskellStopCommands::'); // Re-enable command execution
   // Add GHC to path automatically because it does not add until the end of the step and we check the path.
 
