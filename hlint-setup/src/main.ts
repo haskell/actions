f8a1177a4d14b02e3c8326454ea463f412093837
Pavel Zwerschke <pavelzw@gmail.com>
2023-05-11 19:15:58 +0200

    use tsup and eslint for hlint-setup

--- hlint-setup/src/main.ts
+++ hlint-setup/src/main.ts
@@ -1,51 +1,61 @@
-import * as core from '@actions/core'
-import * as tc from '@actions/tool-cache'
-import * as os from 'os'
-import * as path from 'path'
+import * as os from 'os';
+import * as path from 'path';
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
 
 const TAR_ARCHIVE = {ext: 'tar.gz', extract: tc.extractTar};
 const ZIP_ARCHIVE = {ext: 'zip', extract: tc.extractZip};
 
 interface PlatformArchiveConfig {
-  toolType: {pkgPlatform: string, ext: string},
-  archiveType: {ext: string, extract: (archivePath: string, toDir: string) => Promise<string>},
-};
+  toolType: {pkgPlatform: string; ext: string};
+  archiveType: {
+    ext: string;
+    extract: (archivePath: string, toDir: string) => Promise<string>;
+  };
+}
 
 // process.platform are (linux, darwin, win32, …)
 // hlint releases are (linux, osx, windows, …)
 const HLINT_PLATFORM_ARCHIVE_CONFIG: Record<string, PlatformArchiveConfig> = {
   darwin: {toolType: {pkgPlatform: 'osx', ext: ''}, archiveType: TAR_ARCHIVE},
   linux: {toolType: {pkgPlatform: 'linux', ext: ''}, archiveType: TAR_ARCHIVE},
-  win32: {toolType: {pkgPlatform: 'windows', ext: 'exe'}, archiveType: ZIP_ARCHIVE},
+  win32: {
+    toolType: {pkgPlatform: 'windows', ext: 'exe'},
+    archiveType: ZIP_ARCHIVE
+  }
 };
 
 // os.arch() gives x64. The package archs are identified as x86_64.
 // At least as of hlint 3.1.6, all platforms are x86_64.
 const HLINT_ARCH_CONFIG: Record<string, string> = {
-  x64: 'x86_64',
+  x64: 'x86_64'
 };
 
 interface ToolConfig {
-  arch: string,
-  name: string,
-  exeName: string,
-  platform: string,
-  version: string,
+  arch: string;
+  name: string;
+  exeName: string;
+  platform: string;
+  version: string;
 }
 
 interface ArchiveConfig {
-  url: string,
-  fileName: string,
-  extractionSubdir: string,
-  extract: (archivePath: string, toDir: string) => Promise<string>,
+  url: string;
+  fileName: string;
+  extractionSubdir: string;
+  extract: (archivePath: string, toDir: string) => Promise<string>;
 }
 
 interface HLintReleaseConfig {
-  tool: ToolConfig,
-  archive: ArchiveConfig,
-};
+  tool: ToolConfig;
+  archive: ArchiveConfig;
+}
 
-function mkHlintReleaseConfig(nodeOsPlatform: string, nodeArch: string, hlintVersion: string): HLintReleaseConfig {
+function mkHlintReleaseConfig(
+  nodeOsPlatform: string,
+  nodeArch: string,
+  hlintVersion: string
+): HLintReleaseConfig {
   const config = HLINT_PLATFORM_ARCHIVE_CONFIG[nodeOsPlatform];
   if (!config) {
     throw Error(`Invalid platform for hlint: ${nodeOsPlatform}`);
@@ -55,7 +65,10 @@ function mkHlintReleaseConfig(nodeOsPlatform: string, nodeArch: string, hlintVer
     throw Error(`Unsupported architecture hlint: ${nodeArch}`);
   }
 
-  const {toolType: {pkgPlatform, ext: exeExt}, archiveType: {ext: archiveExt, extract}} = config;
+  const {
+    toolType: {pkgPlatform, ext: exeExt},
+    archiveType: {ext: archiveExt, extract}
+  } = config;
 
   const toolName = 'hlint';
   const releaseName = `${toolName}-${hlintVersion}`;
@@ -66,7 +79,7 @@ function mkHlintReleaseConfig(nodeOsPlatform: string, nodeArch: string, hlintVer
       name: toolName,
       exeName: exeExt ? `${toolName}.${exeExt}` : toolName,
       platform: nodeOsPlatform,
-      version: hlintVersion,
+      version: hlintVersion
     },
     archive: {
       // URL for downloading the archive
@@ -77,34 +90,48 @@ function mkHlintReleaseConfig(nodeOsPlatform: string, nodeArch: string, hlintVer
       extractionSubdir: releaseName,
       // Function of (archiveFile, extractTo).
       // Archive files will be in `${extractTo}/${extractionSubdir}`.
-      extract,
-    },
+      extract
+    }
   };
 }
 
-async function getHlintExistingPath(hlintReleaseConfig: HLintReleaseConfig): Promise<string> {
+async function getHlintExistingPath(
+  hlintReleaseConfig: HLintReleaseConfig
+): Promise<string> {
   const {tool} = hlintReleaseConfig;
   return tc.find(tool.name, tool.version, tool.arch);
 }
 
-async function downloadHlint(hlintReleaseConfig: HLintReleaseConfig): Promise<string> {
+async function downloadHlint(
+  hlintReleaseConfig: HLintReleaseConfig
+): Promise<string> {
   const {tool, archive} = hlintReleaseConfig;
   const {extract: extractArchive, extractionSubdir} = archive;
   const archivePath = await tc.downloadTool(archive.url);
   const extractedFolder = await extractArchive(archivePath, os.homedir());
   const releaseFolder = path.join(extractedFolder, extractionSubdir);
-  const cachedDir = await tc.cacheDir(releaseFolder, tool.name, tool.version, tool.arch);
+  const cachedDir = await tc.cacheDir(
+    releaseFolder,
+    tool.name,
+    tool.version,
+    tool.arch
+  );
   return cachedDir;
 }
 
-async function findOrDownloadHlint(hlintReleaseConfig: HLintReleaseConfig): Promise<string> {
+async function findOrDownloadHlint(
+  hlintReleaseConfig: HLintReleaseConfig
+): Promise<string> {
   const existingHlintDir = await getHlintExistingPath(hlintReleaseConfig);
   if (existingHlintDir) {
     core.debug(`Found cached hlint at ${existingHlintDir}`);
     return existingHlintDir;
   } else {
     core.debug('hlint not cached, so attempting to download');
-    return core.group('Downloading hlint', async () => await downloadHlint(hlintReleaseConfig));
+    return core.group(
+      'Downloading hlint',
+      async () => await downloadHlint(hlintReleaseConfig)
+    );
   }
 }
 
@@ -117,13 +144,21 @@ const OUTPUT_KEY_HLINT_VERSION = 'version';
 
 async function run() {
   try {
-    const hlintVersion = core.getInput(INPUT_KEY_HLINT_VERSION) || HLINT_DEFAULT_VERSION;
-    const config = mkHlintReleaseConfig(process.platform, os.arch(), hlintVersion);
+    const hlintVersion =
+      core.getInput(INPUT_KEY_HLINT_VERSION) || HLINT_DEFAULT_VERSION;
+    const config = mkHlintReleaseConfig(
+      process.platform,
+      os.arch(),
+      hlintVersion
+    );
     const hlintDir = await findOrDownloadHlint(config);
     core.addPath(hlintDir);
     core.info(`hlint ${config.tool.version} is now set up at ${hlintDir}`);
     core.setOutput(OUTPUT_KEY_HLINT_DIR, hlintDir);
-    core.setOutput(OUTPUT_KEY_HLINT_PATH, path.join(hlintDir, config.tool.exeName));
+    core.setOutput(
+      OUTPUT_KEY_HLINT_PATH,
+      path.join(hlintDir, config.tool.exeName)
+    );
     core.setOutput(OUTPUT_KEY_HLINT_VERSION, config.tool.version);
   } catch (error) {
     core.setFailed(error instanceof Error ? error : String(error));
---
+++ hlint-setup/tsup.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'tsup';
+
+export default defineConfig({
+  entry: {
+    index: 'src/main.ts'
+  },
+  dts: false,
+  clean: true,
+  target: 'es2020',
+  format: ['cjs'],
+  sourcemap: true,
+  minify: false,
+  // need to bundle dependencies because they aren't available otherwise when run inside the action
+  noExternal: [
+    '@actions/core',
+    '@actions/tool-cache'
+  ]
+});
